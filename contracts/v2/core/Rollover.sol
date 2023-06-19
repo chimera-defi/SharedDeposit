e0fd44df0451b1e71e5a50eeab67a78693e183a2
Chimera Defi <chimera_defi@protonmail.com>
2023-06-19 17:44:22 -0400

    refactor withdrawals, rollover, add re-entrancy gaurd

--- contracts/v2/core/Rollover.sol
+++ contracts/v2/core/Rollover.sol
@@ -1,8 +1,7 @@
 pragma solidity 0.8.20;
 
 // SPDX-License-Identifier: MIT
-import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeERC20, SafeMath, IERC20, RedemptionsBase} from "../../lib/RedemptionsBase.sol";
 
 /// @title Rollover - ERC20 token to ETH redemption contract
 /// @author @ChimeraDefi - sharedstake.org
@@ -15,73 +14,26 @@ import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeE
     TODO Docs
     Test on goerli deployed at https://goerli.etherscan.io/address/0x4db116ad5cca33ba5d2956dba80d56f27b6b2455
 **/
-contract Rollover {
+contract Rollover is RedemptionsBase {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    error ContractBalanceTooLow();
-    error UserAmountIsZero();
-    struct UserEntry {
-        uint256 amount;
-    }
-
-    mapping(address => UserEntry) public userEntries;
-    uint256 public totalOut;
-    uint256 public immutable virtualPrice;
-    IERC20 public immutable vEth2Token;
     IERC20 public immutable newToken;
 
     constructor(
         address _underlying,
         address _newToken,
         uint256 _virtualPrice
-    ) payable {
-        vEth2Token = IERC20(_underlying);
+    ) payable RedemptionsBase(_underlying, _virtualPrice) {
         newToken = IERC20(_newToken);
-        virtualPrice = _virtualPrice;
     }
 
-    function deposit(uint256 amount) external {
-        // vEth2 transfer from returns true otherwise reverts
-        if (vEth2Token.transferFrom(msg.sender, address(this), amount)) {
-            _stakeForWithdrawal(msg.sender, amount);
-        }
-    }
-
-    function withdraw() external {
-        uint256 amt = userEntries[msg.sender].amount;
-        delete userEntries[msg.sender];
-
-        vEth2Token.transferFrom(address(this), msg.sender, amt);
-    }
-
-    function redeem() external {
+    function _redeem(uint256 amountToReturn) internal override {
         // make sure user has tokens to redeem offchain first by looking at userEntries otherwise this will just waste gas
-        address usr = msg.sender;
-        uint256 amountToReturn = _getAmountGivenShares(userEntries[usr].amount, virtualPrice);
-        if (amountToReturn == 0) {
-            revert UserAmountIsZero();
-        }
         if (amountToReturn > newToken.balanceOf(address(this))) {
             revert ContractBalanceTooLow();
         }
-        delete userEntries[usr];
-        totalOut += amountToReturn;
 
         newToken.transfer(msg.sender, amountToReturn);
     }
-
-    function _stakeForWithdrawal(address sender, uint256 amount) internal {
-        UserEntry memory ue = userEntries[sender];
-        ue.amount = ue.amount.add(amount);
-        userEntries[sender] = ue;
-    }
-
-    function _getAmountGivenShares(uint256 shares, uint256 _vp) internal pure returns (uint256) {
-        return shares.mul(_vp).div(1e18);
-    }
-
-    receive() external payable {} // solhint-disable-line
-
-    fallback() external payable {} // solhint-disable-line
 }
