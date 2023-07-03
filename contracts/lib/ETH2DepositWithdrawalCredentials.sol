69850663f47f7c2d34b6a158b3308752ae60fdab
Chimera Defi <chimera_defi@protonmail.com>
2023-07-02 21:23:56 -0400

    switch to assembly for batchdeposits. may need to revert

--- contracts/lib/ETH2DepositWithdrawalCredentials.sol
+++ contracts/lib/ETH2DepositWithdrawalCredentials.sol
@@ -6,44 +6,53 @@ import {IDepositContract} from "../interfaces/IDepositContract.sol";
 /// @notice A contract for holding a eth2 validator withrawal pubkey
 /// @dev Downstream contract needs to implement who can set the withdrawal address and set it
 contract ETH2DepositWithdrawalCredentials {
-    uint256 internal constant _depositAmount = 32 ether;
-    IDepositContract internal constant depositContract =  IDepositContract(0x00000000219ab540356cBB839Cbe05303d7705Fa); // solhint-disable-line
-    bytes public curr_withdrawal_pubkey; // Pubkey for ETH 2.0 withdrawal creds
+  uint256 internal constant _depositAmount = 32 ether;
+  IDepositContract internal constant depositContract =  IDepositContract(0x00000000219ab540356cBB839Cbe05303d7705Fa); // solhint-disable-line
+  bytes public curr_withdrawal_pubkey; // Pubkey for ETH 2.0 withdrawal creds
 
-    event WithdrawalCredentialSet(bytes _withdrawalCredential);
+  event WithdrawalCredentialSet(bytes _withdrawalCredential);
 
-    constructor() {} // solhint-disable-line
+  constructor() {} // solhint-disable-line
 
-    /// @notice A more streamlined variant of batch deposit for use with preset withdrawal addresses
-    ///         Submit index-matching arrays that form Phase 0 DepositData objects.
-    ///         Will create a deposit transaction per index of the arrays submitted.
-    ///
-    /// @param pubkeys - An array of BLS12-381 public keys.
-    /// @param signatures - An array of BLS12-381 signatures.
-    /// @param depositDataRoots - An array of the SHA-256 hash of the SSZ-encoded DepositData object.
-    function _batchDeposit(
-        bytes[] calldata pubkeys,
-        bytes[] calldata signatures,
-        bytes32[] calldata depositDataRoots
-    ) internal {
-        uint256 loops = pubkeys.length;
-        bytes memory wpk = curr_withdrawal_pubkey;
+  /// @notice A more streamlined variant of batch deposit for use with preset withdrawal addresses
+  ///         Submit index-matching arrays that form Phase 0 DepositData objects.
+  ///         Will create a deposit transaction per index of the arrays submitted.
+  ///
+  /// @param pubkeys - An array of BLS12-381 public keys.
+  /// @param signatures - An array of BLS12-381 signatures.
+  /// @param depositDataRoots - An array of the SHA-256 hash of the SSZ-encoded DepositData object.
+  function _batchDeposit(
+      bytes[] calldata pubkeys,
+      bytes[] calldata signatures,
+      bytes32[] calldata depositDataRoots
+  ) internal {
+      // optimizations https://ethereum.stackexchange.com/questions/113221/what-is-the-purpose-of-unchecked-in-solidity
+      // https://medium.com/@bloqarl/solidity-gas-optimization-tips-with-assembly-you-havent-heard-yet-1381c77ff078
+      // 30m gas / block roughly, say 10m max used so 100 validators a batch max 
+      // each deposit call costs roughly 128k https://etherscan.io/tx/0xa2acf6e6bde99b532125cc8026cd88eea345f296968ce732556945ab4705d03e
 
-        // Loop through DepositData arrays submitting deposits
-        for (uint256 i = 0; i < loops; i++) {
-            depositContract.deposit{value: _depositAmount}(
-                pubkeys[i],
-                wpk,
-                signatures[i],
-                depositDataRoots[i]
-            );
-        }
-    }
+      bytes memory wpk = curr_withdrawal_pubkey;
+      uint i = pubkeys.length;
+      while (i > 0) {
+          depositContract.deposit{value: _depositAmount}(
+              pubkeys[i],
+              wpk,
+              signatures[i],
+              depositDataRoots[i]
+          );
 
-    /// @notice sets curr_withdrawal_pubkey to be used when deploying validators
-    function _setWithdrawalCredential(bytes memory _new_withdrawal_pubkey) internal {
-        curr_withdrawal_pubkey = _new_withdrawal_pubkey;
+          // jump into assembly to do some quick cheap gas math
+          assembly {
+              i := sub(i, 1)
+          }
+      }
+  }
+  
 
-        emit WithdrawalCredentialSet(_new_withdrawal_pubkey);
-    }
+  /// @notice sets curr_withdrawal_pubkey to be used when deploying validators
+  function _setWithdrawalCredential(bytes memory _new_withdrawal_pubkey) internal {
+      curr_withdrawal_pubkey = _new_withdrawal_pubkey;
+
+      emit WithdrawalCredentialSet(_new_withdrawal_pubkey);
+  }
 }
