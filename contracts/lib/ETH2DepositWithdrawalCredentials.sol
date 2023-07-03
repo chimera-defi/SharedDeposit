b745a495da4a6dbdd13b124b5b160a867c709510
Chimera Defi <chimera_defi@protonmail.com>
2023-07-02 20:28:15 -0400

    refactor minter

--- contracts/lib/ETH2DepositWithdrawalCredentials.sol
+++ contracts/lib/ETH2DepositWithdrawalCredentials.sol
@@ -1,24 +1,20 @@
 pragma solidity 0.8.20;
 
 import {IDepositContract} from "../interfaces/IDepositContract.sol";
-import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
 /// @title  A contract for holding a eth2 validator withrawal pubkey
 /// @author @chimeraDefi
 /// @notice A contract for holding a eth2 validator withrawal pubkey
 /// @dev Downstream contract needs to implement who can set the withdrawal address and set it
 contract ETH2DepositWithdrawalCredentials {
-    using SafeMath for uint256;
-
     uint256 internal constant _depositAmount = 32 ether;
-    IDepositContract internal constant depositContract =  IDepositContract(0x00000000219ab540356cBB839Cbe05303d7705Fa);
+    IDepositContract internal constant depositContract =  IDepositContract(0x00000000219ab540356cBB839Cbe05303d7705Fa); // solhint-disable-line
     bytes public curr_withdrawal_pubkey; // Pubkey for ETH 2.0 withdrawal creds
 
     event WithdrawalCredentialSet(bytes _withdrawalCredential);
 
-    constructor() {}
+    constructor() {} // solhint-disable-line
 
-    /// @notice A more streamlined variant of batch deposit for use with preset withdrawal addresses 
+    /// @notice A more streamlined variant of batch deposit for use with preset withdrawal addresses
     ///         Submit index-matching arrays that form Phase 0 DepositData objects.
     ///         Will create a deposit transaction per index of the arrays submitted.
     ///
@@ -31,13 +27,13 @@ contract ETH2DepositWithdrawalCredentials {
         bytes32[] calldata depositDataRoots
     ) internal {
         uint256 loops = pubkeys.length;
-        bytes memory withdraw_pubkey = curr_withdrawal_pubkey;
+        bytes memory wpk = curr_withdrawal_pubkey;
 
         // Loop through DepositData arrays submitting deposits
         for (uint256 i = 0; i < loops; i++) {
             depositContract.deposit{value: _depositAmount}(
                 pubkeys[i],
-                withdraw_pubkey,
+                wpk,
                 signatures[i],
                 depositDataRoots[i]
             );
--- contracts/v2/core/SharedDepositMinterV2.sol
+++ contracts/v2/core/SharedDepositMinterV2.sol
@@ -23,10 +23,9 @@ pragma solidity 0.8.20;
 */
 import {IFeeCalc} from "../../interfaces/IFeeCalc.sol";
 import {IERC20MintableBurnable} from "../../interfaces/IERC20MintableBurnable.sol";
-import {IWSGEth} from "../../interfaces/IWSGEth.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 import {AccessControlEnumerable} from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
@@ -35,73 +34,71 @@ import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {ETH2DepositWithdrawalCredentials} from "../../lib/ETH2DepositWithdrawalCredentials.sol";
 
 contract SharedDepositMinterV2 is AccessControlEnumerable, Pausable, ReentrancyGuard, ETH2DepositWithdrawalCredentials {
-    using SafeMath for uint256;
-    /* ========== STATE VARIABLES ========== */
-    uint256 public adminFee;
-    uint256 public numValidators;
-    uint256 public costPerValidator;
+  using SafeMath for uint256;
+  /* ========== STATE VARIABLES ========== */
+  uint256 public adminFee;
+  uint256 public numValidators;
+  uint256 public costPerValidator;
 
-    // The validator shares created by this shared stake contract. 1 share costs >= 1 eth
-    uint256 public curValidatorShares; //initialized to 0
+  // The validator shares created by this shared stake contract. 1 share costs >= 1 eth
+  uint256 public curValidatorShares; //initialized to 0
 
-    // The number of times the deposit to eth2 contract has been called to create validators
-    uint256 public validatorsCreated; //initialized to 0
+  // The number of times the deposit to eth2 contract has been called to create validators
+  uint256 public validatorsCreated; //initialized to 0
 
-    // Total accrued admin fee
-    uint256 public adminFeeTotal; //initialized to 0
+  // Total accrued admin fee
+  uint256 public adminFeeTotal; //initialized to 0
 
-    // Its hard to exactly hit the max deposit amount with small shares. this allows a small bit of overflow room
-    // Eth in the buffer cannot be withdrawn by an admin, only by burning the underlying token via a user withdraw
-    uint256 public buffer;
+  // Its hard to exactly hit the max deposit amount with small shares. this allows a small bit of overflow room
+  // Eth in the buffer cannot be withdrawn by an admin, only by burning the underlying token via a user withdraw
+  uint256 public buffer;
 
-    // Flash loan tokenomic protection in case of changes in admin fee with future lots
-    bool public refundFeesOnWithdraw; //initialized to false
+  // Flash loan tokenomic protection in case of changes in admin fee with future lots
+  bool public refundFeesOnWithdraw; //initialized to false
 
-    // NEW
+  // NEW
 
-    //errors
-    error AmountTooHigh();
+  //errors
+  error AmountTooHigh();
+  error WithdrawalCredentialsAlreadySet();
 
-    address public LSDTokenAddress;
-    IFeeCalc public FeeCalc;
-    IERC20MintableBurnable public SGETH;
-    IWSGEth public WSGETH;
+  IERC20MintableBurnable private immutable _sgeth;
+  IERC4626 private immutable _wsgeth;
+  IFeeCalc private _feeCalc;
+  mapping(uint16 => uint16) private _called; // trace fn calls and state. e.g. called[0] returns the no. of times the tracing fn has been called
 
-    bytes32 public constant NOR = keccak256("NOR"); // Node operator for deploying validators
-    bytes32 public constant GOV = keccak256("GOV"); // Governance for settings - normally timelock controlled by multisig
+  bytes32 public constant NOR = keccak256("NOR"); // Node operator for deploying validators
+  bytes32 public constant GOV = keccak256("GOV"); // Governance for settings - normally timelock controlled by multisig
 
-    constructor(
-        uint256 _numValidators,
-        uint256 _adminFee,
-        address[] memory addresses
-    ) ETH2DepositWithdrawalCredentials() {
-        FeeCalc = IFeeCalc(addresses[0]);
-        SGETH = IERC20MintableBurnable(addresses[1]);
-        WSGETH = IWSGEth(addresses[2]);
+  constructor(
+    uint256 _numValidators,
+    uint256 _adminFee,
+    address[] memory addresses
+  ) ETH2DepositWithdrawalCredentials() {
+    _feeCalc = IFeeCalc(addresses[0]);
+    _sgeth = IERC20MintableBurnable(addresses[1]);
+    _wsgeth = IERC4626(addresses[2]);
 
-        uint256 MAX_INT = 2**256 - 1;
-        SGETH.approve(address(WSGETH), MAX_INT); // max approve wsgeth for deposit and stake
+    _sgeth.approve(address(_wsgeth), 2**256 - 1); // max approve wsgeth for deposit and stake
 
-        adminFee = _adminFee; // Admin and infra fees
-        numValidators = _numValidators; // The number of validators to create in this lot. Sets a max limit on deposits
+    adminFee = _adminFee; // Admin and infra fees
+    numValidators = _numValidators; // The number of validators to create in this lot. Sets a max limit on deposits
 
-        // Eth in the buffer cannot be withdrawn by an admin, only by burning the underlying token
-        buffer = uint256(10).mul(1e18); // roughly equal to 10 eth.
+    // Eth in the buffer cannot be withdrawn by an admin, only by burning the underlying token
+    buffer = uint256(10).mul(1e18); // roughly equal to 10 eth.
 
-        LSDTokenAddress = address(SGETH);
+    costPerValidator = uint256(32).mul(1e18).add(adminFee);
 
-        costPerValidator = uint256(32).mul(1e18).add(adminFee);
+    _grantRole(NOR, msg.sender);
+    _grantRole(GOV, msg.sender); // deployer will need it to set withdrawal creds. since the non-custodial withdrawal path depends on the minter.
+  }
 
-        _grantRole(NOR, msg.sender);
-        _grantRole(GOV, addresses[3]);
-    }
-
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                         DEPOSIT/WITHDRAWAL LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    // USER INTERACTIONS
-    /*
+  // USER INTERACTIONS
+  /*
         Shares minted = Z
         Principal deposit input = P
         AdminFee = a
@@ -117,167 +114,173 @@ contract SharedDepositMinterV2 is AccessControlEnumerable, Pausable, ReentrancyG
         P = Z - Z*a%
     */
 
-    function deposit() external payable nonReentrant whenNotPaused {
-        SGETH.mint(msg.sender, _depositAccounting());
-    }
+  function deposit() external payable nonReentrant whenNotPaused {
+    _sgeth.mint(msg.sender, _depositAccounting());
+  }
 
-    function depositFor(address dest) external payable nonReentrant whenNotPaused {
-        SGETH.mint(dest, _depositAccounting());
-    }
+  function depositFor(address dest) external payable nonReentrant whenNotPaused {
+    _sgeth.mint(dest, _depositAccounting());
+  }
 
-    function depositAndStake() external payable nonReentrant whenNotPaused {
-        uint256 amt = _depositAccounting();
-        SGETH.mint(address(this), amt);
-        WSGETH.deposit(amt, msg.sender);
-    }
+  function depositAndStake() external payable nonReentrant whenNotPaused {
+    uint256 amt = _depositAccounting();
+    _sgeth.mint(address(this), amt);
+    _wsgeth.deposit(amt, msg.sender);
+  }
 
-    function depositAndStakeFor(address dest) external payable nonReentrant whenNotPaused {
-        uint256 amt = _depositAccounting();
-        SGETH.mint(address(this), amt);
-        WSGETH.deposit(amt, dest);
-    }
+  function depositAndStakeFor(address dest) external payable nonReentrant whenNotPaused {
+    uint256 amt = _depositAccounting();
+    _sgeth.mint(address(this), amt);
+    _wsgeth.deposit(amt, dest);
+  }
 
-    function withdraw(uint256 amount) external nonReentrant whenNotPaused {
-        _withdraw(amount, msg.sender, msg.sender);
-    }
+  function withdraw(uint256 amount) external nonReentrant whenNotPaused {
+    _withdraw(amount, msg.sender, msg.sender);
+  }
 
-    function withdrawTo(uint256 amount, address dest) external nonReentrant whenNotPaused {
-        _withdraw(amount, msg.sender, dest);
-    }
+  function withdrawTo(uint256 amount, address dest) external nonReentrant whenNotPaused {
+    _withdraw(amount, msg.sender, dest);
+  }
 
-    function unstakeAndWithdraw(uint256 amount, address dest) external nonReentrant whenNotPaused {
-        uint256 assets = WSGETH.redeem(amount, address(this), msg.sender);
-        _withdraw(assets, address(this), dest);
-    }
+  function unstakeAndWithdraw(uint256 amount, address dest) external nonReentrant whenNotPaused {
+    uint256 assets = _wsgeth.redeem(amount, address(this), msg.sender);
+    _withdraw(assets, address(this), dest);
+  }
 
-    // migration function to accept old monies and copy over state
-    // users should not use this as it just donates the money without minting veth or tracking donations
-    function donate(uint256 shares) external payable nonReentrant {}
+  // migration function to accept old monies and copy over state
+  // users should not use this as it just donates the money without minting veth or tracking donations
+  function donate(uint256 shares) external payable nonReentrant {} // solhint-disable-line
 
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                             ADMIN LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    // Batch deposit eth to the eth2 contract with preset creds
-    // Data needs to be verified offchain to save gas
-    function batchDepositToEth2(
-        bytes[] calldata pubkeys,
-        bytes[] calldata signatures,
-        bytes32[] calldata depositDataRoots
-    ) external onlyRole(NOR) {
-        if (address(this).balance < _depositAmount.mul(pubkeys.length)) {
-            revert AmountTooHigh(); // Not enough bal in contract to deploy all validators
-        }
-        _batchDeposit(pubkeys, signatures, depositDataRoots);
-        validatorsCreated = validatorsCreated.add(pubkeys.length);
+  // Batch deposit eth to the eth2 contract with preset creds
+  // Data needs to be verified offchain to save gas
+  function batchDepositToEth2(
+    bytes[] calldata pubkeys,
+    bytes[] calldata signatures,
+    bytes32[] calldata depositDataRoots
+  ) external onlyRole(NOR) {
+    if (address(this).balance < _depositAmount.mul(pubkeys.length)) {
+      revert AmountTooHigh(); // Not enough bal in contract to deploy all validators
     }
+    _batchDeposit(pubkeys, signatures, depositDataRoots);
+    validatorsCreated = validatorsCreated.add(pubkeys.length);
+  }
 
-    // Used to migrate state over to new contract
-    function migrateShares(uint256 shares) external onlyRole(GOV) nonReentrant {
-        curValidatorShares = shares;
+  // Used to migrate state over to new contract
+  function migrateShares(uint256 shares) external onlyRole(GOV) nonReentrant {
+    curValidatorShares = shares;
+  }
+
+  // Slashes the onchain staked sgETH to mirror CL validator slashings
+  // modifies wsgeth virtual price
+  function slash(uint256 amt) external onlyRole(GOV) {
+    if (amt > curValidatorShares) {
+      revert AmountTooHigh(); // Cannot slash more than minted
     }
+    _sgeth.burn(address(_wsgeth), amt);
+  }
 
-    // Slashes the onchain staked sgETH to mirror CL validator slashings
-    // modifies wsgeth virtual price
-    function slash(uint256 amt) external onlyRole(GOV) {
-        if (amt > curValidatorShares) {
-            revert AmountTooHigh(); // Cannot slash more than minted
-        }
-        SGETH.burn(address(WSGETH), amt);
+  // Set fee calc address. if addr = 0 then fees are assumed to be 0
+  function setFeeCalc(address _feeCalculatorAddr) external onlyRole(GOV) {
+    _feeCalc = IFeeCalc(_feeCalculatorAddr);
+  }
+
+  function toggleWithdrawRefund() external onlyRole(GOV) {
+    refundFeesOnWithdraw = !refundFeesOnWithdraw;
+  }
+
+  function setNumValidators(uint256 _numValidators) external onlyRole(GOV) {
+    require(_numValidators != 0, "Minimum 1 validator");
+    numValidators = _numValidators;
+  }
+
+  function setWithdrawalCredential(bytes memory _newWithdrawalCreds) external onlyRole(NOR) {
+    // can only be called once.
+    if (_called[0] > 0) {
+      revert WithdrawalCredentialsAlreadySet();
     }
+    _called[0] += 1;
 
-    // Set fee calc address. if addr = 0 then fees are assumed to be 0
-    function setFeeCalc(address _feeCalculatorAddr) external onlyRole(GOV) {
-        FeeCalc = IFeeCalc(_feeCalculatorAddr);
+    _setWithdrawalCredential(_newWithdrawalCreds);
+  }
+
+  function withdrawAdminFee(uint256 amount) external onlyRole(GOV) {
+    address payable sender = payable(msg.sender);
+    if (amount == 0) {
+      amount = adminFeeTotal;
     }
-
-    function toggleWithdrawRefund() external onlyRole(GOV) {
-        refundFeesOnWithdraw = !refundFeesOnWithdraw;
+    if (amount > adminFeeTotal) {
+      revert AmountTooHigh();
     }
+    adminFeeTotal = adminFeeTotal.sub(amount);
+    Address.sendValue(sender, amount);
+  }
 
-    function setNumValidators(uint256 _numValidators) external onlyRole(GOV) {
-        require(_numValidators != 0, "Minimum 1 validator");
-        numValidators = _numValidators;
-    }
-
-    function setWithdrawalCredential(bytes memory _new_withdrawal_pubkey) external onlyRole(GOV) {
-        _setWithdrawalCredential(_new_withdrawal_pubkey);
-    }
-
-    function withdrawAdminFee(uint256 amount) external onlyRole(GOV) {
-        address payable sender = payable(msg.sender);
-        if (amount == 0) {
-            amount = adminFeeTotal;
-        }
-        if (amount > adminFeeTotal) {
-            revert AmountTooHigh();
-        }
-        adminFeeTotal = adminFeeTotal.sub(amount);
-        Address.sendValue(sender, amount);
-    }
-
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                             ACCOUNTING LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    function remainingSpaceInEpoch() external view returns (uint256) {
-        // Helpful view function to gauge how much the user can send to the contract when it is near full
-        uint256 remainingShares = (maxValidatorShares()).sub(curValidatorShares);
-        uint256 valBeforeAdmin = remainingShares.mul(1e18).div(
-            uint256(1).mul(1e18).sub(adminFee.mul(1e18).div(costPerValidator))
-        );
-        return valBeforeAdmin;
+  function remainingSpaceInEpoch() external view returns (uint256) {
+    // Helpful view function to gauge how much the user can send to the contract when it is near full
+    uint256 remainingShares = (maxValidatorShares()).sub(curValidatorShares);
+    uint256 valBeforeAdmin = remainingShares.mul(1e18).div(
+      uint256(1).mul(1e18).sub(adminFee.mul(1e18).div(costPerValidator))
+    );
+    return valBeforeAdmin;
+  }
+
+  function maxValidatorShares() public view returns (uint256) {
+    return uint256(32).mul(1e18).mul(numValidators);
+  }
+
+  function _depositAccounting() internal returns (uint256 value) {
+    // input is whole, not / 1e18 , i.e. in 1 = 1 eth send when from etherscan
+    value = msg.value;
+    uint256 fee;
+
+    if (address(_feeCalc) != address(0)) {
+      (value, fee) = _feeCalc.processDeposit(value, msg.sender);
+      adminFeeTotal = adminFeeTotal.add(fee);
     }
 
-    function maxValidatorShares() public view returns (uint256) {
-        return uint256(32).mul(1e18).mul(numValidators);
+    uint256 newShareTotal = curValidatorShares.add(value);
+
+    if (newShareTotal > buffer.add(maxValidatorShares())) {
+      revert AmountTooHigh();
+    }
+    curValidatorShares = newShareTotal;
+  }
+
+  function _withdrawAccounting(uint256 amount) internal returns (uint256) {
+    uint256 fee;
+    if (address(_feeCalc) != address(0)) {
+      (amount, fee) = _feeCalc.processWithdraw(amount, msg.sender);
+      if (refundFeesOnWithdraw) {
+        adminFeeTotal = adminFeeTotal.sub(fee);
+      } else {
+        adminFeeTotal = adminFeeTotal.add(fee);
+      }
+    }
+    if (address(this).balance < amount.add(adminFeeTotal)) {
+      revert AmountTooHigh();
     }
 
-    function _depositAccounting() internal returns (uint256 value) {
-        // input is whole, not / 1e18 , i.e. in 1 = 1 eth send when from etherscan
-        value = msg.value;
-        uint256 fee;
+    curValidatorShares = curValidatorShares.sub(amount);
+    return amount;
+  }
 
-        if (address(FeeCalc) != address(0)) {
-            (value, fee) = FeeCalc.processDeposit(value, msg.sender);
-            adminFeeTotal = adminFeeTotal.add(fee);
-        }
+  function _withdraw(
+    uint256 amount,
+    address origin,
+    address dest
+  ) internal {
+    _sgeth.burn(origin, amount);
+    uint256 assets = _withdrawAccounting(amount);
 
-        uint256 newShareTotal = curValidatorShares.add(value);
-
-        if (newShareTotal > buffer.add(maxValidatorShares())) {
-            revert AmountTooHigh();
-        }
-        curValidatorShares = newShareTotal;
-    }
-
-    function _withdrawAccounting(uint256 amount) internal returns (uint256) {
-        uint256 fee;
-        if (address(FeeCalc) != address(0)) {
-            (amount, fee) = FeeCalc.processWithdraw(amount, msg.sender);
-            if (refundFeesOnWithdraw) {
-                adminFeeTotal = adminFeeTotal.sub(fee);
-            } else {
-                adminFeeTotal = adminFeeTotal.add(fee);
-            }
-        }
-        if (address(this).balance < amount.add(adminFeeTotal)) {
-            revert AmountTooHigh();
-        }
-
-        curValidatorShares = curValidatorShares.sub(amount);
-        return amount;
-    }
-
-    function _withdraw(
-        uint256 amount,
-        address origin,
-        address dest
-    ) internal {
-        SGETH.burn(origin, amount);
-        uint256 assets = _withdrawAccounting(amount);
-
-        address payable recv = payable(dest);
-        Address.sendValue(recv, assets);
-    }
+    address payable recv = payable(dest);
+    Address.sendValue(recv, assets);
+  }
 }
