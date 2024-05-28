e102c9d8dda0297d25d33c9946b9cd3efea5bed7
devlancer412 <ienjoycoding4u@gmail.com>
2024-05-27 19:29:40 -0700

    added e2e test

--- test/v2/core/sgETH.spec.js
+++ test/v2/core/sgETH.spec.js
@@ -53,4 +53,33 @@ describe("SgETH.sol", () => {
       .and.to.be.emit(sgEth, "RoleRevoked")
       .withArgs(ethers.constants.HashZero, deployer.address, deployer.address);
   });
+
+  it("e2e test", async () => {
+    const WSGETH = await ethers.getContractFactory("WSGETH");
+    const wsgETH = await WSGETH.deploy(sgEth.address, 24 * 60 * 60);
+    await wsgETH.deployed();
+
+    const feeCalc = ethers.constants.AddressZero; // not sure if it is right
+    const numValidators = 1000;
+    const adminFee = 0;
+
+    const addresses = [
+      feeCalc,
+      sgEth.address,
+      wsgETH.address,
+      ethers.constants.AddressZero, // using dummy address
+      ethers.constants.AddressZero, // using dummy address
+    ];
+
+    const Minter = await ethers.getContractFactory("SharedDepositMinterV2");
+    const minter = await Minter.deploy(numValidators, adminFee, addresses);
+    await minter.deployed();
+
+    await sgEth.removeMinter(deployer.address);
+    await sgEth.transferOwnership(minter.address);
+
+    await expect(sgEth.connect(deployer).transferOwnership(alice.address)).to.be.revertedWith(
+      `AccessControl: account ${deployer.address.toLowerCase()} is missing role ${ethers.constants.HashZero}`,
+    );
+  });
 });
