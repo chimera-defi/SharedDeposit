a4c4ec070ea7009fa5d45bc47ea0f8618775f12a
devlancer412 <ienjoycoding4u@gmail.com>
2024-05-29 06:12:33 -0700

    e2e test finished

--- test/v2/core/sgETH.spec.js
+++ test/v2/core/sgETH.spec.js
@@ -3,11 +3,11 @@ const {expect} = require("chai");
 const {parseEther} = require("ethers/lib/utils");
 
 describe("SgETH.sol", () => {
-  let sgEth, deployer, alice;
+  let sgEth, deployer, alice, multiSig;
   let MINTER_ROLE;
 
   beforeEach(async () => {
-    const [owner, addr1] = await ethers.getSigners();
+    const [owner, addr1, addr2] = await ethers.getSigners();
 
     const SgETH = await ethers.getContractFactory("SgETH");
     sgEth = await SgETH.deploy([]);
@@ -15,6 +15,7 @@ describe("SgETH.sol", () => {
 
     deployer = owner;
     alice = addr1;
+    multiSig = addr2;
 
     MINTER_ROLE = await sgEth.MINTER();
   });
@@ -61,22 +62,44 @@ describe("SgETH.sol", () => {
     const wsgETH = await WSGETH.deploy(sgEth.address, 24 * 60 * 60);
     await wsgETH.deployed();
 
-    const feeCalc = ethers.constants.AddressZero; // not sure if it is right
+    const splitterAddresses = [deployer.address, multiSig.address, wsgETH.address];
+    const splitterValues = [6, 3, 31];
+
+    const PaymentSplitter = await ethers.getContractFactory("PaymentSplitter");
+    const paymentSplitter = await PaymentSplitter.deploy(splitterAddresses, splitterValues);
+    await paymentSplitter.deployed();
+
+    const rolloverVirtual = "1080000000000000000";
+    const vETH2Addr = "0x898bad2774eb97cf6b94605677f43b41871410b1";
+
+    const Withdrawals = await ethers.getContractFactory("Withdrawals");
+    const withdrawals = await Withdrawals.deploy(vETH2Addr, rolloverVirtual);
+    await withdrawals.deployed();
+
     const numValidators = 1000;
     const adminFee = 0;
 
     const addresses = [
-      feeCalc, // fee calculator
+      paymentSplitter.address, // fee splitter
       sgEth.address, // sgETH address
       wsgETH.address, // wsgETH address
-      ethers.constants.AddressZero, // government address
-      ethers.constants.AddressZero, // deposit contract
+      multiSig.address, // government address
+      ethers.constants.AddressZero, // deposit contract address - can't find deposit contract - using dummy address
     ];
 
     // add secondary minter contract / eoa
     const Minter = await ethers.getContractFactory("SharedDepositMinterV2");
     const minter = await Minter.deploy(numValidators, adminFee, addresses);
     await minter.deployed();
+
+    const RewardsReceiver = await ethers.getContractFactory("RewardsReceiver");
+    const rewardsReceiver = await RewardsReceiver.deploy(withdrawals.address, [
+      sgEth.address,
+      wsgETH.address,
+      paymentSplitter.address,
+      minter.address,
+    ]);
+    await rewardsReceiver.deployed();
 
     // deployer can't mint
     await expect(sgEth.connect(deployer).mint(deployer.address, parseEther("1"))).to.be.revertedWith(
@@ -98,9 +121,9 @@ describe("SgETH.sol", () => {
     await sgEth.removeMinter(deployer.address);
     // add secondary owner
     // revoke deployer admin rights
-    await expect(sgEth.transferOwnership(minter.address))
+    await expect(sgEth.transferOwnership(multiSig.address))
       .to.be.emit(sgEth, "RoleGranted")
-      .withArgs(ethers.constants.HashZero, minter.address, deployer.address)
+      .withArgs(ethers.constants.HashZero, multiSig.address, deployer.address)
       .and.to.be.emit(sgEth, "RoleRevoked")
       .withArgs(ethers.constants.HashZero, deployer.address, deployer.address);
 
