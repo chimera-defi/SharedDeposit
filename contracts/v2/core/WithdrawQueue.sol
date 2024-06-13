da23a71efab9e1c41d2c00e88f847e6c86d131b1
devlancer412 <ienjoycoding4u@gmail.com>
2024-06-12 18:21:37 -0700

    handle feedback

--- contracts/v2/core/WithdrawQueue.sol
+++ contracts/v2/core/WithdrawQueue.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.20;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {ERC4626, ERC20} from "solmate/src/mixins/ERC4626.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+import {SharedDepositMinterV2} from "./SharedDepositMinterV2.sol";
 
 contract WithdrawQueue is AccessControl, Pausable, ReentrancyGuard {
     using Address for address payable;
@@ -12,25 +15,51 @@ contract WithdrawQueue is AccessControl, Pausable, ReentrancyGuard {
     struct Queue {
         address to;
         uint256 amount;
+        address token;
+        uint256 tokenAmount;
     }
+
+    address public immutable sgEth;
+    address public immutable wsgEth;
+    SharedDepositMinterV2 public immutable minter;
 
     uint256 public constant CHUNK_SIZE = 32 ether;
 
-    uint256 front;
-    uint256 end;
-    uint256 lockedFront;
-    uint256 lockedAmount;
+    uint256 private front;
+    uint256 private end;
+    uint256 private lockedFront;
+    uint256 private lockedAmount;
 
     mapping(uint256 => Queue) public queue;
 
     error NoAvailableQueue();
 
-    constructor() {
+    constructor(address _sgEth, address _wsgEth, address _minter) {
         grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        sgEth = _sgEth;
+        wsgEth = _wsgEth;
+        minter = SharedDepositMinterV2(_minter);
     }
 
-    function push(address to) external payable nonReentrant {
-        Queue memory newWithdraw = Queue({to: to, amount: msg.value});
+    function push(address to) external payable {
+        _push(to, msg.value, address(0), msg.value);
+    }
+
+    function pushSgEth(address to, uint256 amount) external {
+        ERC20(sgEth).transferFrom(msg.sender, address(this), amount);
+        _push(to, amount, sgEth, amount);
+    }
+
+    function pushWsgEth(address to, uint256 amount) external {
+        ERC20(wsgEth).transferFrom(msg.sender, address(this), amount);
+
+        uint256 redeemable = ERC4626(wsgEth).previewRedeem(amount);
+        _push(to, redeemable, wsgEth, amount);
+    }
+
+    function _push(address to, uint256 amount, address token, uint256 tokenAmount) internal nonReentrant {
+        Queue memory newWithdraw = Queue({to: to, amount: msg.value, token: token, tokenAmount: tokenAmount});
 
         queue[end] = newWithdraw;
         lockedAmount += msg.value;
@@ -48,6 +77,12 @@ contract WithdrawQueue is AccessControl, Pausable, ReentrancyGuard {
         }
 
         for (uint256 i = front; i < lockedFront; ) {
+            if (queue[i].token == sgEth) {
+                minter.withdraw(queue[i].tokenAmount);
+            } else if (queue[i].token == wsgEth) {
+                minter.unstakeAndWithdraw(queue[i].tokenAmount, address(this));
+            }
+
             payable(queue[i].to).sendValue(queue[i].amount);
 
             unchecked {
