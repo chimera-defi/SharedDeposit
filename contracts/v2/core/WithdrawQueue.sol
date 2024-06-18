63d584956b54f0af30baa3b9ac02202bc3b29310
devlancer412 <ienjoycoding4u@gmail.com>
2024-06-18 10:13:58 -0700

    bugfix

--- contracts/v2/core/WithdrawQueue.sol
+++ contracts/v2/core/WithdrawQueue.sol
@@ -10,15 +10,23 @@ import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {SharedDepositMinterV2} from "./SharedDepositMinterV2.sol";
 
 contract WithdrawQueue is AccessControl, Pausable, ReentrancyGuard {
+    struct Request {
+        address requester;
+        uint256 shares;
+    }
     SharedDepositMinterV2 public immutable MINTER;
-    address public immutable SGETH;
     address public immutable WSGETH;
 
     uint256 public constant REDEEM_THRESOLD = 32 ether;
 
-    // This code snippet is incomplete pseudocode used for example only and is no way intended to be used in production or guaranteed to be secure
-    mapping(address => uint256) public pendingRedeemRequest;
+    uint256 internal requestBack;
+    uint256 internal totalPendingRequest;
+    uint256 internal requestFront;
+    mapping(uint256 => Request) internal requests;
     mapping(address => uint256) public claimableRedeemRequest;
+
+    // This code snippet is incomplete pseudocode used for example only and is no way intended to be used in production or guaranteed to be secure
+    // mapping(address => uint256) public pendingRedeemRequest;
 
     mapping(address requester => mapping(address operator => bool)) public isOperator;
 
@@ -34,6 +42,7 @@ contract WithdrawQueue is AccessControl, Pausable, ReentrancyGuard {
 
     error InvalidAmount();
     error PermissionDenied();
+    error InsufficientBalance();
 
     modifier onlyOwnerOrOperator(address owner) {
         if (owner != msg.sender && !isOperator[owner][msg.sender]) {
@@ -42,36 +51,44 @@ contract WithdrawQueue is AccessControl, Pausable, ReentrancyGuard {
         _;
     }
 
-    constructor(address _minter, address _sgEth, address _wsgEth) {
+    constructor(address _minter, address _wsgEth) {
         MINTER = SharedDepositMinterV2(_minter);
-        SGETH = _sgEth;
         WSGETH = _wsgEth;
 
         uint256 maxUint256 = 2 ** 256 - 1;
 
         IERC20(WSGETH).approve(_minter, maxUint256);
-        IERC20(SGETH).approve(_minter, maxUint256);
     }
 
     function requestRedeem(
         uint256 shares,
         address requester,
         address owner
-    ) external onlyOwnerOrOperator(owner) returns (uint256 requestId) {
+    ) external onlyOwnerOrOperator(owner) nonReentrant returns (uint256 requestId) {
         if (shares == 0) {
             revert InvalidAmount();
         }
 
-        requestId = 0; // no requestId associated with this request
+        requestId = requestBack; // no requestId associated with this request
+        requestBack++;
 
         IERC20(WSGETH).transferFrom(owner, address(this), shares); // asset here is the Vault underlying asset
 
-        pendingRedeemRequest[requester] += shares;
+        totalPendingRequest += shares;
+        requests[requestId] = Request({requester: requester, shares: shares});
 
-        uint256 assets = IERC4626(WSGETH).previewRedeem(shares);
+        uint256 assets = IERC4626(WSGETH).previewRedeem(totalPendingRequest);
         if (assets >= REDEEM_THRESOLD) {
-            claimableRedeemRequest[requester] += pendingRedeemRequest[requester];
-            pendingRedeemRequest[requester] = 0;
+            uint256 i = requestFront;
+            requestFront = requestBack;
+            for (; i < requestFront; ) {
+                claimableRedeemRequest[requests[i].requester] += requests[i].shares;
+
+                unchecked {
+                    ++i;
+                }
+            }
+            totalPendingRequest = 0;
         }
 
         emit RedeemRequest(requester, owner, requestId, msg.sender, shares);
@@ -86,7 +103,7 @@ contract WithdrawQueue is AccessControl, Pausable, ReentrancyGuard {
         uint256 shares,
         address receiver,
         address requester
-    ) external onlyOwnerOrOperator(requester) returns (uint256 assets) {
+    ) external onlyOwnerOrOperator(requester) nonReentrant returns (uint256 assets) {
         if (shares == 0) {
             revert InvalidAmount();
         }
@@ -95,7 +112,20 @@ contract WithdrawQueue is AccessControl, Pausable, ReentrancyGuard {
 
         assets = IERC4626(WSGETH).previewRedeem(shares);
 
-        MINTER.unstakeAndWithdraw(shares, receiver);
+        uint256 queueBalance = address(this).balance;
+        uint256 minterBalance = address(MINTER).balance;
+
+        if (queueBalance + minterBalance < assets) {
+            revert InsufficientBalance();
+        }
+
+        if (assets <= queueBalance) {
+            payable(receiver).transfer(assets);
+        } else {
+            payable(receiver).transfer(queueBalance);
+            uint256 remainingShares = IERC4626(WSGETH).convertToShares(assets - queueBalance);
+            MINTER.unstakeAndWithdraw(remainingShares, receiver);
+        }
 
         emit Redeem(requester, receiver, shares, assets);
     }
@@ -105,4 +135,18 @@ contract WithdrawQueue is AccessControl, Pausable, ReentrancyGuard {
         emit OperatorSet(msg.sender, operator, approved);
         return true;
     }
+
+    function pendingRedeemRequest(address requester) public view returns (uint256 shares) {
+        for (uint256 i = 0; i < requestBack; ) {
+            if (requests[i].requester == requester) {
+                shares += requests[i].shares;
+            }
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    receive() external payable {} // solhint-disable-line
+    fallback() external payable {} // solhint-disable-line
 }
