25513612bfbf8f6a199a482e338fab294a3fd4cf
devlancer412 <ienjoycoding4u@gmail.com>
2024-06-25 12:37:17 -0700

    used constant multisig

--- deploy/05_withdrawalQueue.ts
+++ deploy/05_withdrawalQueue.ts
@@ -1,18 +1,15 @@
 import {DeployFunction} from "hardhat-deploy/types";
 import Ship from "../utils/ship";
 import {
-  SgETH,
-  SgETH__factory,
   SharedDepositMinterV2,
   SharedDepositMinterV2__factory,
   WSGETH,
   WSGETH__factory,
   WithdrawalQueue__factory,
 } from "../types";
-import {ZeroAddress} from "ethers";
 
 const func: DeployFunction = async hre => {
-  const {deploy, connect, accounts} = await Ship.init(hre);
+  const {deploy, connect} = await Ship.init(hre);
 
   const wsgEth = (await connect(WSGETH__factory)) as WSGETH;
   const minter = (await connect(SharedDepositMinterV2__factory)) as SharedDepositMinterV2;
